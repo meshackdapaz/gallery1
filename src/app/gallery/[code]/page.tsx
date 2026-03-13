c264d12ece34084c588b849f17b34d5506527b22
meshackurassa2-design <meshackurassa2@gmail.com>
2026-03-13 09:21:35 +0300

    Fix static export: Add dummy param and dynamicParams=false to dynamic routes

--- src/app/gallery/[code]/page.tsx
+++ src/app/gallery/[code]/page.tsx
@@ -1,8 +1,10 @@
 import GalleryClient from './GalleryClient';
 
 export function generateStaticParams() {
-  return [];
+  return [{ code: 'preview' }];
 }
+
+export const dynamicParams = false;
 
 export default async function GalleryPage({ params }: { params: Promise<{ code: string }> }) {
   return <GalleryClient params={params} />;
--- src/app/gallery/[code]/slideshow/page.tsx
+++ src/app/gallery/[code]/slideshow/page.tsx
@@ -1,8 +1,10 @@
 import SlideshowClient from './SlideshowClient';
 
 export function generateStaticParams() {
-  return [];
+  return [{ code: 'preview' }];
 }
+
+export const dynamicParams = false;
 
 export default async function SlideshowPage({ params }: { params: Promise<{ code: string }> }) {
   return <SlideshowClient params={params} />;
