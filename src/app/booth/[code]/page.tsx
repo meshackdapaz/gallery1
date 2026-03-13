4ab40d676e0a588fefec8f14c80f7b6d308883a4
meshackurassa2-design <meshackurassa2@gmail.com>
2026-03-13 17:45:20 +0300

    Fix static export build error by adding generateStaticParams to Booth page

--- src/app/booth/[code]/page.tsx
+++ src/app/booth/[code]/page.tsx
@@ -1,5 +1,11 @@
 import BoothClient from './BoothClient';
 
+export function generateStaticParams() {
+  return [{ code: 'preview' }];
+}
+
+export const dynamicParams = false;
+
 export default function BoothPage({ params }: { params: Promise<{ code: string }> }) {
   return <BoothClient params={params} />;
 }
